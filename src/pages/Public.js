5aca0eb716c5c937bdea0833ed5cff8ee55f2e7f
AlexandraLukacs <lszandika15@gmail.com>
2024-11-19 11:45:33 +0100

    public

--- src/pages/Public.js
+++ src/pages/Public.js
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react'
 import { ApiContext } from '../context/ApiContext'
+import Termek from '../components/Termek'
 
 export default function Public() {
   const {termekLista}=useContext(ApiContext)
     return (
     <div className=''>
         <main>
-            <article></article>
+            <article>
+                 <Termek />
+            </article>
             <aside></aside>
         </main>
     </div>
