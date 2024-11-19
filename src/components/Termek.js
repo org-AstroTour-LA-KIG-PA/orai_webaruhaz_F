2102c426b8928051ba78cdd538e9f138fc6ff22b
AlexandraLukacs <lszandika15@gmail.com>
2024-11-19 11:48:42 +0100

    Termek

--- src/components/Termek.js
+++ src/components/Termek.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function Termek() {
+export default function Termek(props) {
   return (
     <div className='card'>
-        <h4 className='card-title'></h4>
-        <p></p>
-        <p></p>
-        <p></p>
+        <h4 className='card-title'>{props.termek.nev}</h4>
+        <p>{props.termek.leiras}</p>
+        <p>{props.termek.kategoria}</p>
+        <p>{props.termek.ar}</p>
         <button>Kosárba</button>
     </div>
   )
