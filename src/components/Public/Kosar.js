772ddbc95220b365163e6bde600978162ded1e33
AlexandraLukacs <lszandika15@gmail.com>
2024-11-26 16:08:46 +0100

    kosar kezdet

--- src/components/Public/Kosar.js
+++ src/components/Public/Kosar.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function Kosar() {
+export default function Kosar(props) {
   return (
-    <div>Kosar</div>
+    <div>
+      <h4>{props.termek.nev}</h4>
+      <p>{props.termek.ar}</p>
+      <p>🗑️</p>
+    </div>
   )
 }
