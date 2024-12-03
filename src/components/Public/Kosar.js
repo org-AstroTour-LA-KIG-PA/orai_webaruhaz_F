c116875dc71fd06929cba4a4137dbb6cf75c3a9c
AlexandraLukacs <lszandika15@gmail.com>
2024-12-03 09:23:19 +0100

    public, kosar

--- src/components/Public/Kosar.js
+++ src/components/Public/Kosar.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Kosar(props) {
   return (
     <div>
-      <h4>{props.termek.nev}</h4>
-      <p>{props.termek.ar}</p>
+      
       <p>🗑️</p>
     </div>
   )
