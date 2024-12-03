c180a6ca5b5b86914ddf611acc11a48dff1fc628
AlexandraLukacs <lszandika15@gmail.com>
2024-12-03 10:58:20 +0100

    css

--- src/components/Public/Kosar.js
+++ src/components/Public/Kosar.js
@@ -4,7 +4,6 @@ export default function Kosar(props) {
   return (
     <div>
       
-      <p>🗑️</p>
     </div>
   )
 }
--- src/components/Public/Termek.js
+++ src/components/Public/Termek.js
@@ -3,14 +3,14 @@ import "./Termek.css"
 
 export default function Termek(props) {
   return (
-    <div className=' col-lg-5 col-md-4 '>
-      
-     
+    <div className=' col-lg-5 col-md-4'>
         <div className='card'>
             <h4 className='card-title'>{props.termek.nev}</h4>
             <p>{props.termek.leiras}</p>
-            <p>{props.termek.kategoria}</p>
-            <p>{props.termek.ar} Ft</p>
+            <div className='row'>
+            <p className='col-sm-6'>Kategória: {props.termek.kategoria}</p>
+            <p className='col-sm-6'>Ár: {props.termek.ar} Ft</p>
+            </div>
             <button>Kosárba</button>
         </div>
     </div>
